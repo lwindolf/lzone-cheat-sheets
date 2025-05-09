a04eb78f86dcfda86cc4b1e635d27a623b4c21eb
Create Index Workflow <noreply@example.com>
2025-05-09 17:57:13 +0200

    Simplified build.sh only for README

--- build.sh
+++ build.sh
@@ -3,91 +3,12 @@
 set -euo pipefail
 
 # Validate JSON
-jq . cheat-sheets.json >/dev/null
 jq . extra-cheat-sheets.json >/dev/null
 
 # Reset stuff we want to modify
 if [ "${GITHUB_RUN_NUMBER-}" = "" ]; then
-	git checkout README.md cheat-sheets.json
+	git checkout README.md
 fi
-
-# Update default cheat sheet JSON with document structure
-cheat_sheets() {
-  (
-  cat <<'EOT'
-  async function run() {
-    var cheatsheets = require('./cheat-sheets.json');
-
-    for(const name of Object.keys(cheatsheets)) {
-      let repo = cheatsheets[name];
-      let regex = new RegExp((repo.filePattern?repo.filePattern:"^(.+).md$"));
-
-      // fetch default branch first
-      let data = await fetch(`https://api.github.com/repos/${repo.github}`)
-          .then((response) => response.json())
-          .then((data) => { repoInfo = data; return fetch(`https://api.github.com/repos/${repo.github}/git/trees/${data.default_branch}?recursive=1`)})
-          .then((response) => response.json());
-
-      cheatsheets[name].documents = data.tree
-        .map((a) => a.path)
-        .filter((path) => regex.test(path))
-        .map((path) => {
-          var tmp = path.match(regex);
-          return { name: tmp[1], path };
-        });
-
-    }
-    console.log(JSON.stringify(cheatsheets));
-  }
-
-  run();
-EOT
-  ) | node | jq >cheat-sheets.json.new && mv cheat-sheets.json.new cheat-sheets.json
-}
-
-# Build lunr search index
-build_search_index() {
-  (
-  cat <<'EOT'
-  async function run() {
-    var cheatsheets = require('./cheat-sheets.json');
-    var docs = {};
-
-    for(const name of Object.keys(cheatsheets)) {
-      let repo = cheatsheets[name];
-
-      // Add entry for section title
-      const j = Object.keys(docs).length;
-      docs[j] = {
-          doc: `${name}`,
-          id: j,
-          title: `${name}`,
-          content: "",
-          relUrl: `/#/${name}`,
-          url: `/#/${name}`
-      }
-
-      // Add 1st level child entries
-      repo?.documents.forEach((d) => {
-          const j = Object.keys(docs).length;
-          docs[j] = {
-              doc: `${name}`,
-              id: j,
-              title: `${d.name}`,
-              content: "",
-              relUrl: `/#/${name}/${d.name}`,
-              url: `/#/${name}/${d.name}`
-          }
-      });
-
-    }
-    console.log(JSON.stringify(docs));
-  }
-
-  run();
-EOT
-  ) | node | jq >search-index.json
-}
 
 # Update README with a list of all cheat sheets defined
 readme_update() {
